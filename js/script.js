0a4f17117501eb72a9a2c17a98a9c8f999a80a77
Jorge Acosta <66733491+jorgeacosta19@users.noreply.github.com>
2020-12-02 11:16:05 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
-const $instagram = document.getElementById("instagram");
-const $facebook = document.getElementById("facebook");
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+
+
+    if (prevScrollpos > currentScrollPos) {
+        // this is scrolling up
+        document.getElementById("navbar").style.top = "0";
+        document.getElementById("logo-vl").style.visibility = "visible";
+        document.getElementById("name-vl").style.display = "block";
+        document.getElementById("navbar").style.maxHeight = "100%";
+        document.getElementById("navbar").style.backgroundColor = "rgb(98, 98, 98)";
+
+    } else {
+        // this is scrolling down
+        document.getElementById("navbar").style.top = "-0.4rem";
+        document.getElementById("navbar").style.maxHeight = "10%";
+        document.getElementById("logo-vl").style.visibility = "hidden";
+        document.getElementById("name-vl").style.display = "none";
+        document.getElementById("navbar").style.backgroundColor = "rgb(98, 98, 98)";
+
+    }
+    prevScrollpos = currentScrollPos;
+}
